5ffe38799d706f94054de71a5fc408eb0095522b
DESKTOP-7C1Q2D6\Nguyen Thi Thu Hong <thuhong05022003@gmail.com>
2023-09-29 11:46:33 +0700

    fisrt commit

---
+++ src/main/Main.java
@@ -0,0 +1,51 @@
+package main;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+
+public class Main extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setExtendedState(JFrame.MAXIMIZED_BOTH); // Đặt cửa sổ thành fullscreen
+        setUndecorated(true); // Loại bỏ tiêu đề và khung viền của cửa sổ
+		setBounds(100, 100, 602, 364);
+	
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(92, 50, 263, 245);
+		contentPane.add(panel);
+		panel.setLayout(null);
+	}
+}
